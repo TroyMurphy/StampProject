5d7be1d8f437abcba3958eeb177b27699db7820b
TroyMurphy <troymurphy4@gmail.com>
2014-11-30 19:49:07 -0700

    Added classes

--- TroyProject/main.py
+++ TroyProject/main.py
@@ -2,15 +2,18 @@ def main():
     """
     This is the main function.
     The key is to put the least amount of code here as possible.
+    
     Gather just enough information to create the Copy instance and run self.stamp()
     self.stamp will be the most complex function since it should handle stamping the image and then
     outputting the pdf to a file location.
+    
+    
     """
     #Get necessary inputs
     #This is what could use tkinter. 
     pass
 
-    #Create instance of PDF
+    #Create instance of PDF using input data
     
     #Create copy instances using filters
     
--- TroyProject/models/Copy.py
+++ TroyProject/models/Copy.py
@@ -4,13 +4,20 @@ class Copy(object):
     to a pdf document it can create a new PDF with
     pages which match a given criteria are stamped with
     the provided stamps
+    
+    Copy should include a function to serialize and save
+    itself to a file with pickle so it does not have to be recreated each time
     """
     def __init__(self, pdf_file_path,
                  filter1, filter2, condition,
-                 stamps):
+                 stamps, initial_offsets=(0,0)):
         self.file_path = pdf_file_path
         self.filter1 = filter1
         self.filter2 = filter2
         self.condition = condition
         self.stamps = stamps
-        
+        self.offsets = initial_offsets
+    
+    def serialize(self):
+        pass
+    
