e312ff86a23bc72cb0df83b497819cb18feff8b2
TroyMurphy <troymurphy4@gmail.com>
2014-11-30 18:05:52 -0700

    Added doc to main.py

--- TroyProject/main.py
+++ TroyProject/main.py
@@ -1,7 +1,20 @@
 def main():
+    """
+    This is the main function.
+    The key is to put the least amount of code here as possible.
+    Gather just enough information to create the Copy instance and run self.stamp()
+    self.stamp will be the most complex function since it should handle stamping the image and then
+    outputting the pdf to a file location.
+    """
     #Get necessary inputs
+    #This is what could use tkinter. 
     pass
 
+    #Create instance of PDF
+    
+    #Create copy instances using filters
+    
+    #Have each copy run self.stamp
 
 
 
